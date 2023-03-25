e8c6a7af8afeea99502c35d843ddb4c0541fc0a4
Johan <joenmodu99@gmail.com>
2023-03-25 14:18:50 -0400

    feat: users connection actions added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,67 @@
 const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const bingo = require('./controllers/bingo')
+let activeUsers = []
+let gameMode = ''
 
-const config = require('./server/config')
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+})
 
-require('./database')
+io.sockets.on('connection', async (socket) => {
+  socket.on('set-mode', (mode) => {
+    if (!gameMode) {
+      gameMode = mode
+    }
 
-const app = config(express())
+    io.emit('lobby-closed', {
+      mode: gameMode
+    })
+  })
 
-app.listen(app.get('port'), () => {
-  console.log('Server on port', app.get('port'))
+  socket.on('request-game', (user) => {
+    const newUser = {
+      id: socket.id,
+      name: user.playerName
+    }
+
+    activeUsers.push(newUser)
+
+    socket.emit('joined-game', {
+      otherPlayers: [...activeUsers],
+      player: newUser
+    })
+
+    io.emit('player-connected', {
+      ...newUser
+    })
+
+    const newTable = bingo.getCardBoard()
+
+    socket.emit('table-assigned', {
+      id: socket.id,
+      table: newTable
+    })
+  })
+
+  socket.on('answer-table', (answer) => {
+    console.log(answer)
+  })
+
+  socket.on('disconnect', () => {
+    let userDisconnected = activeUsers.find((user) => user.id === socket.id)
+
+    activeUsers = activeUsers.filter((user) => user.id !== socket.id)
+
+    io.emit('player-disconnected', { ...userDisconnected })
+  })
+})
+
+server.listen(3000, () => {
+  console.log('listening on *:3000')
 })
